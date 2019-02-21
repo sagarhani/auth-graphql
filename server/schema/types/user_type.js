18897f69395a46b263b097f89f0cc047c268fc78
sagarhani <sagarhani33@gmail.com>
2019-02-21 23:57:06 +0530

    User Type

---
+++ server/schema/types/user_type.js
@@ -0,0 +1,11 @@
+const graphql = require('graphql');
+const {
+    GraphQLObjectType,
+    GraphQLString
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+    
+});
+
+module.exports = UserType;
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://sagarhani:password123@ds147225.mlab.com:47225/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
