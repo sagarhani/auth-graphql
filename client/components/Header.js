0105593c5edf18a7411dd71f115d3101d17163fb
sagarhani <sagarhani33@gmail.com>
2019-02-28 00:14:11 +0530

    Authentication states

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import query from '../queries/CurrentUser';
 
 class Header extends React.Component {
+    renderButtons() {
+        const { loading, user } = this.props.data;
+        if (this.props.data.loading) {
+            return <div />;
+        }
+        if (user) {
+            return <div>Logout</div>;
+        } else {
+            return (
+                <div>You're not signed in.</div>
+            );
+        }
+    }
+
     render() {
-        console.log(this.props.data); 
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to="/" className="brand-logo left">
+                        Home
+                    </Link>
+                    <ul className="right">
+                        {this.renderButtons()}
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
