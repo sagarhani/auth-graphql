4fef2b8160ec2c949828fcb07bf2a9452fbb2735
sagarhani <sagarhani33@gmail.com>
2019-02-28 01:17:49 +0530

    Login form

---
+++ client/components/LoginForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AuthForm from './AuthForm';
+
+class LoginForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Login</h3>
+                <AuthForm />
+            </div>
+        );
+    }
+}
+
+export default LoginForm;
