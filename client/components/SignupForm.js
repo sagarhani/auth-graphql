ea0e674c9a4b79f764d53253b5aab3e0414ca448
sagarhani <sagarhani33@gmail.com>
2019-03-01 23:35:09 +0530

    Signup mutation

---
+++ client/components/SignupForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import AuthForm from './AuthForm';
+import { graphql } from 'react-apollo';
+import mutation from '../mutations/Signup';
+
+class SignupForm extends React.Component {
+    onSubmit({ email, password }) {
+        this.props.mutate({
+            variables: { email, password }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Sign Up</h3>
+                <AuthForm errors={[]} onSubmit={this.onSubmit.bind(this)} />
+            </div>
+        );
+    }
+}
+
+export default graphql(mutation)(SignupForm);
