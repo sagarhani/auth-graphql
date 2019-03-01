88914c82f0a2d8e4ffd45fbc3aebc752ae7ce993
sagarhani <sagarhani33@gmail.com>
2019-03-01 23:16:26 +0530

    Error handling

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -29,9 +29,13 @@ class AuthForm extends React.Component {
                         <label>Password</label>
                         <input 
                             placeholder="Password"
+                            type="password"
                             value={this.state.password}
                             onChange={e => this.setState({ password: e.target.password })}
                         />
+                    </div>
+                    <div className="errors">
+                        {this.props.errors.map(error => <div key={error}>{error}</div>)}
                     </div>
                     <button className="btn">Submit</button>
                 </form>
